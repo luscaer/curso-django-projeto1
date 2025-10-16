6796630067301dfc09e910610de215735da55061
luscaer <lerc.rj@hotmail.com>
2025-10-16 09:38:35 -0300

    feat: recipe details page

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={'name': 'Lucas Emanuel',},status=200)
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'Lucas Emanuel',},status=200)
