eee94340da37b04515bd7f89343a8e9770f28a72
luscaer <lerc.rj@hotmail.com>
2025-10-21 09:53:14 -0300

    feat: simple detail page

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from utils.recipes.factory import make_recipe
+
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name': 'Lucas Emanuel',},status=200)
+    return render(request, 'recipes/pages/home.html', context={'recipes': [make_recipe() for recipe in range(10)],},status=200)
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'Lucas Emanuel',},status=200)
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True
+    },status=200)
