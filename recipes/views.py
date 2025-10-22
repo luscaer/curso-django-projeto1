4ab03608cdcabba06ad5e35372a8bea54a20d545
luscaer <lerc.rj@hotmail.com>
2025-10-22 12:59:11 -0300

    .

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from recipes.models import Recipe
 
 from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'recipes': [make_recipe() for recipe in range(10)],},status=200)
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+        })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
         'is_detail_page': True
-    },status=200)
+    })
