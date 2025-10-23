1ecfeb7a7bd3b26fc81ee4342ee1e187e1f07a5b
luscaer <lerc.rj@hotmail.com>
2025-10-23 12:01:37 -0300

    feat: Category page

--- recipes/views.py
+++ recipes/views.py
@@ -7,7 +7,10 @@ from utils.recipes.factory import make_recipe
 # Create your views here.
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+            is_published=True
+        ).order_by('-id')
+    
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
         })
@@ -17,3 +20,13 @@ def recipe(request, id):
         'recipe': make_recipe(),
         'is_detail_page': True
     })
+    
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+    ).order_by('-id')
+    
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        })
